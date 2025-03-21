a39bf02ddd3762981421a3b8bfa42a9787a7c801
Kuroashi97 <aldo.barone31@gmail.com>
2025-03-21 15:02:40 +0100

    Delete esercizioCatalogo directory

--- esercizioCatalogo/src/main/java/com/catalogo/esercizioCatalogo/repository/RuoloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.catalogo.esercizioCatalogo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.catalogo.esercizioCatalogo.model.Ruolo;
-
-public interface RuoloRepository extends CrudRepository<Ruolo, Long> {
-	Ruolo findByNome(String nome);
-}
--- esercizioCatalogo/src/main/java/com/catalogo/esercizioCatalogo/repository/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.catalogo.esercizioCatalogo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.catalogo.esercizioCatalogo.model.Utente;
-
-public interface UtenteRepository extends CrudRepository<Utente, Long> {
-	Utente findByUsername(String username);
-}
